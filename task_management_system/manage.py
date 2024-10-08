c44c086ea77087a601d19b94ee5a3d64545b13b9
hongquan <danghongquanvtc1111@gmail.com>
2024-10-08 10:57:34 +0700

    upd

--- task_management_system/manage.py
+++ task_management_system/manage.py
@@ -15,6 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    port = os.getenv('PORT', '8000')
     execute_from_command_line(sys.argv)
 
 
